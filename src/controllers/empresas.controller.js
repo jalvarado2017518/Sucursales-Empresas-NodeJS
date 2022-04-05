9fc893ff0f2c80ed9acf61693a49543fdd41ba41
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-04-05 15:43:45 -0600

    Actualizando funciones

---
+++ src/controllers/empresas.controller.js
@@ -0,0 +1,151 @@
+const Usuarios = require('../models/empresas.model');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('../services/jwt')
+
+
+function UsuarioInicial() {
+    Usuarios.find({ rol: "SuperAdmin", usuario: "SuperAdmin" }, (err, usuarioEncontrado) => {
+      if (usuarioEncontrado.length == 0) {
+        bcrypt.hash("123456", null, null, (err, passwordEncriptada) => {
+          Usuarios.create({
+            usuario: "SuperAdmin",
+            password: passwordEncriptada,
+            rol: "SuperAdmin",
+          });
+        });
+      }
+    });
+  }
+  
+//Login
+ function Login(req, res) {
+    var parametros = req.body;
+    Empresas.findOne({ email: parametros.email }, (err, empresaEncontrada) => {
+        if (err) return res.status(500).send({ mensaje: "Error en la petición" });
+        if (empresaEncontrada) {
+             // COMPARO CONTRASENA SIN ENCRIPTAR CON LA ENCRIPTADA
+            bcrypt.compare(parametros.password, empresaEncontrada.password,
+                (err, verificacionPassword) => {
+                        // VERIFICO SI EL PASSWORD COINCIDE EN BASE DE DATOS
+                    if (verificacionPassword) {
+                             // SI EL PARAMETRO OBTENERTOKEN ES TRUE, CREA EL TOKEN
+                        if (parametros.obtenerToken === 'true') {
+                            return res.status(200)
+                                .send({ token: jwt.crearToken(empresaEncontrada) })
+                        } else {
+                            empresaEncontrada.password = undefined;
+                            return res.status(200)
+                                .send({ empresa: empresaEncontrada })
+                        }
+                    } else {
+                        return res.status(500)
+                            .send({ mensaje: "La contraseña no coincide, intente de nuevo" });
+                    }
+                })
+
+        } else {
+            return res.status(500)
+                .send({ mensaje: "El correo no existe, intente de nuevo" })
+        }
+    })
+}
+//Agregar Empresa
+function agregarEmpresa(req, res) {
+    var parametros = req.body;
+    var empresasModel = new Empresas();
+
+    if (parametros.nombre && parametros.email && parametros.password) {
+        empresasModel.nombre = parametros.nombre;
+        empresasModel.email = parametros.email;
+        empresasModel.password = parametros.password;
+        empresasModel.rol = 'Empresa';
+
+        Empresas.find({ email: parametros.email }, (err, empresaEncotrada) => {
+            if (empresaEncotrada.length == 0) {
+
+                bcrypt.hash(parametros.password, null, null, (err, passwordEncriptada) => {
+                    empresasModel.password = passwordEncriptada;
+
+                    empresasModel.save((err, empresaGuardada) => {
+                        if (err) return res.status(500)
+                            .send({ mensaje: "Error en la peticion" });
+                        if (!empresaGuardada) return res.status(500)
+                            .send({ mensaje: "Error al agregar empresa" });
+                        return res.status(200).send({ empresa: empresaGuardada });
+                    });
+                });
+            } else {
+                return res.status(500)
+                    .send({ mensaje: "Este correo ya existe" });
+            }
+        })
+    }
+}
+
+//Editar Empresa
+function editarEmpresa(req, res) {
+    var idEmpresa = req.params.idEmpresa;
+    var parametros = req.body;
+
+    if (req.user.rol == 'Empresa') {
+        if (parametros.rol) {
+            return res.status(500).send({ message: "No eres Admin no puedes editar" })
+        } else {
+            Empresas.findByIdAndUpdate({ _id: req.user.sub }, parametros, { new: true }, (err, empresaActualizada) => {
+                if (err) return res.status(500).send({ message: "Error en la peticion" });
+                if (!empresaActualizada) return res.status(404).send({ message: "Error en la petición" });
+
+                return res.status(200).send({ empresa: empresaActualizada });
+            });
+        }
+    } else {
+        Empresas.findById(idEmpresa, (err, empresaEncontrada) => {
+            if (err) return res.status(500).send({ message: "Error en la peticion" });
+            if (!empresaEncontrada) return res.status(500).send({ message: "Error en la petición" });
+                
+            if (empresaEncontrada.rol == 'Empresa') {
+                Empresas.findByIdAndUpdate({ _id: idEmpresa }, parametros, { new: true }, (err, empresaActualizada) => {
+                    if (err) return res.status(500).send({ message: "Error en la peticion" });
+                    if (!empresaActualizada) return res.status(404).send({ message: "Error no puedes editar" });
+
+                    return res.status(200).send({ empresa: empresaActualizada });
+                });
+            } else {
+                if (idEmpresa == req.user.sub) {
+                    if (!parametros.rol) {
+                        Empresas.findByIdAndUpdate({ _id: idEmpresa }, parametros, { new: true }, (err, empresaActualizada) => {
+                            if (err) return res.status(500).send({ message: "Error en la peticion" });
+                            if (!empresaActualizada) return res.status(404).send({ message: "Error, no puedes editar" });
+
+                            return res.status(200).send({ empresa: empresaActualizada });
+                        });
+                    } else {
+                        return res.status(500).send({ mensaje: "No puedes editar el Rol" })
+                    }
+                } else {
+                    return res.status(500).send({ mensaje: "No puedes editar esta empresa, intenta de nuevo" });
+                }
+            }
+        })
+
+    }
+
+}
+//Eliminar
+function eliminarEmpresa(req, res) {
+    var idUser = req.params.idEmpresa;
+
+    Empresas.findByIdAndDelete(idUser, (err, empresaEliminada) => {
+        if (err) return res.status(500).send({ mensaje: "Error en la petición" });
+        if (!empresaEliminada) return res.status(500).send({ mensaje: "Error al eliminar esta empresa, intenta de nuevo" });
+        return res.status(200).send({ empresa: empresaEliminada });
+    })
+}
+
+module.exports = {
+    usuarioInicial,
+    Login,
+    agregarEmpresa,
+    editarEmpresa,
+    eliminarEmpresa
+}
