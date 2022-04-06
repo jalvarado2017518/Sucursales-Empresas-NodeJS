21669eccd4860ea174c9a5766eaf2eacfb4c09ee
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-04-05 21:45:37 -0600

    Actualizando

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var app = express();
 // IMPORTACIONES RUTAS
 const UsuarioRutas = require('./src/routes/usuarios.routes');
 const ProductosRutas = require('./src/routes/productos.routes');
+const SucursalesRutas = require('./src/routes/sucursales.routes');
 
 // MIDDLEWARES -> INTERMEDIARIOS
 app.use(express.urlencoded({ extended: false }));
@@ -15,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 
 // CARGA DE RUTAS localhost:3000/api/obtenerProductos
-app.use('/api', UsuarioRutas, ProductosRutas);
+app.use('/api', UsuarioRutas, ProductosRutas, SucursalesRutas);
 
 
 module.exports = app;
