9c551c7eea2bec9483dbcc6d234fff5fbab6fafc
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-05-18 15:47:58 -0600

    actualizando productos sucursales

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const EmpresaRutas = require('./src/routes/empresas.routes');
 //const UsuarioRutas = require('./src/routes/usuarios.routes');
 const ProductosRutas = require('./src/routes/productos.routes');
 const SucursalesRutas = require('./src/routes/sucursales.routes');
-
+const ProductosSucursales = require('./src/routes/productosSucursales.routes');
 // MIDDLEWARES -> INTERMEDIARIOS
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -18,7 +18,6 @@ app.use(cors());
 
 // CARGA DE RUTAS localhost:3000/api/obtenerProductos
 app.use('/api', EmpresaRutas);
-app.use('/api', ProductosRutas, SucursalesRutas);
-
+app.use('/api', ProductosRutas, SucursalesRutas, ProductosSucursales);
 
 module.exports = app;
