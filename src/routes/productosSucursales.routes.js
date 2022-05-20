e86f57780bf60e0438bdb553dc1d0d28015c569e
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-05-20 10:35:55 -0600

    Arreglando funcion

--- src/routes/productosSucursales.routes.js
+++ src/routes/productosSucursales.routes.js
@@ -6,7 +6,7 @@ const api = express.Router();
 
 
 api.get('/obtenerProductosSucursales/:idSucursal', md_autenticacion.Auth, productoSucursalesControlador.obtenerProductosSucursales);
-api.put('/agregarProductos/:idProducto', md_autenticacion.Auth, productoSucursalesControlador.agregarProductoSucursal);
+api.put('/agregarProductoSucursal/:idProducto', md_autenticacion.Auth, productoSucursalesControlador.agregarProductoSucursal);
 api.put('/modificarStock/:idProducto', md_autenticacion.Auth, productoSucursalesControlador.modificarStock);
 api.delete('/eliminarProductosSucursales/:idProducto', md_autenticacion.Auth, productoSucursalesControlador.eliminarProductoSucursal);
 
