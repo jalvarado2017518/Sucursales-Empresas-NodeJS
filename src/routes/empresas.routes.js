2b941db9ea28b5baf03b8c834ee1551e500382c7
Julio Eduardo Gustavo Alvarado Ramirez <88795773+jalvarado2017518@users.noreply.github.com>
2022-05-22 18:41:28 -0600

    last commit

--- src/routes/empresas.routes.js
+++ src/routes/empresas.routes.js
@@ -9,8 +9,9 @@ api.post('/login', empresaControlador.Login);
 api.get('/empresas',md_autenticacion.Auth, empresaControlador.ObtenerEmpresas);
 api.get('/empresas/:idEmpresa', empresaControlador.ObtenerEmpresaId);
 api.post('/agregarEmpresas', empresaControlador.agregarEmpresa);
-api.put('/editarEmpresa/:idEmpresa',  empresaControlador.editarEmpresa);
+api.put('/editarEmpresa/:idEmpresa',md_autenticacion.Auth,  empresaControlador.editarEmpresa);
 api.delete('/eliminarEmpresa/:idEmpresa',  empresaControlador.eliminarEmpresa);
+api.put('/editarEmpresaAdmin/:idEmpresa',  empresaControlador.editarEmpresaAdmin);
 
 module.exports = api;
 
